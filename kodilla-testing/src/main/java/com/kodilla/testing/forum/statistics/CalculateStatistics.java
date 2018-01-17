810198feb699eaa078595d73cea7bd87d0fdc0da
programtica <kondmo@gmail.com>
2018-01-18 00:27:51 +0100

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/CalculateStatistics.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/CalculateStatistics.java
@@ -1,6 +1,102 @@
 package com.kodilla.testing.forum.statistics;
 
-public class CalculateStatistics
-{
+import java.util.List;
 
+class CalculateStatistics implements Statistics
+{
+    private Statistics statistics;
+
+    public CalculateStatistics(Statistics statistics)
+    {
+        this.statistics = statistics;
+    }
+
+    private List<String> userList;
+
+    private int counterOfPosts;
+    private int counterOfComments;
+
+    private double averageQuantityOfPosts;
+    private double averageQuantityOfComments;
+    private double averageQuantityOfCommentsFromPosts;
+
+    public double averageQuantityOfPostsByUsers()
+    {
+        return averageQuantityOfPosts;
+    }
+
+    public double averageQuantityOfCommentsByUsers()
+    {
+        return averageQuantityOfComments;
+    }
+
+    public double averageQuantityOfCommentsByPosts()
+    {
+        return averageQuantityOfCommentsFromPosts;
+    }
+
+    @Override
+    public List<String> userNames()
+    {
+        userList = statistics.userNames();
+        return userList;
+    }
+
+    @Override
+    public int postCount()
+    {
+        counterOfPosts = statistics.postCount();
+        return counterOfPosts;
+    }
+
+    @Override
+    public int commentsCount()
+    {
+        counterOfComments = statistics.commentsCount();
+        return counterOfComments;
+    }
+
+    public void calculateAdvStatistics(Statistics statistics)
+    {
+        if(statistics.userNames().size() !=0 && statistics.postCount() != 0)
+        {
+            averageQuantityOfPosts = statistics.postCount()/(statistics.userNames().size());
+
+        }
+
+        else
+        {
+            averageQuantityOfPosts = 0;
+
+        }
+
+        if(statistics.userNames().size() !=0 && statistics.commentsCount() != 0)
+        {
+            averageQuantityOfComments = statistics.commentsCount()/(statistics.userNames().size());
+        }
+
+        else
+        {
+            averageQuantityOfComments = 0;
+        }
+
+        if(statistics.postCount() != 0 && statistics.commentsCount() !=0)
+        {
+            averageQuantityOfCommentsFromPosts = statistics.postCount()/(statistics.commentsCount());
+        }
+
+        else
+        {
+            averageQuantityOfCommentsFromPosts = 0;
+        }
+    }
+
+    public void showStatistics()
+    {
+        calculateAdvStatistics(statistics);
+        System.out.println("We are presenting a results!");
+        System.out.println(averageQuantityOfPostsByUsers());
+        System.out.println(averageQuantityOfCommentsByUsers());
+        System.out.println(averageQuantityOfCommentsByPosts());
+    }
 }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
@@ -0,0 +1,127 @@
+package com.kodilla.testing.forum.statistics;
+
+import org.junit.*;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class StatisticsTestSuite
+{
+    private static int counterOfTests = 0;
+
+    @Before
+    public void startATest()
+    {
+        counterOfTests++;
+        System.out.println("Start a test #" + counterOfTests);
+    }
+
+    @BeforeClass
+    public static void startOfTests()
+    {
+        System.out.println("Starting a tests!");
+    }
+
+    @AfterClass
+    public static void endOfTests()
+    {
+        System.out.println("We finished all tests!");
+    }
+
+    @Test
+    public void testPfStatisticsWhenNullData()
+    {
+        //Given
+        Statistics statisticsInMock = mock(Statistics.class);
+        int quantityOfPosts = 0;
+        int quantityOfComments = 0;
+
+        ArrayList<String> quantityOfUsers = new ArrayList<>();
+
+        //When
+        CalculateStatistics nullData = new CalculateStatistics(statisticsInMock);
+        nullData.calculateAdvStatistics(statisticsInMock);
+
+        //Then
+        Assert.assertEquals(0, nullData.averageQuantityOfPostsByUsers(), 1.0);
+        Assert.assertEquals(0, nullData.averageQuantityOfCommentsByPosts(), 1.0);
+        Assert.assertEquals(0, nullData.averageQuantityOfCommentsByPosts(), 1.0);
+    }
+
+    @Test
+    public void testOfStatisticsWhenIsFirstTypeOfInformation()
+    {
+        //Given
+        Statistics statisticsInMock = mock(Statistics.class);
+
+        int quantityOfPosts = 1000;
+        int quantityOfComments = 20;
+
+        ArrayList<String> quantityOfUsers = new ArrayList<>();
+
+        for(int i=0; i<1000; i++)
+        {
+            quantityOfUsers.add("User: " + i);
+        }
+        when(statisticsInMock.postCount()).thenReturn(quantityOfPosts);
+        when(statisticsInMock.commentsCount()).thenReturn(quantityOfComments);
+        when(statisticsInMock.userNames()).thenReturn(quantityOfUsers);
+
+        //When
+        CalculateStatistics morePostsThanComments = mock(CalculateStatistics.class);
+        morePostsThanComments.calculateAdvStatistics(statisticsInMock);
+
+        //Then
+        int averageOfPostsWhichMustBeExpected = statisticsInMock.postCount() / statisticsInMock.userNames().size();
+        double postsByUser = morePostsThanComments.averageQuantityOfCommentsByPosts();
+        Assert.assertEquals(averageOfPostsWhichMustBeExpected, postsByUser, 1);
+
+        int averageOfCommentsWhichMustBeExpected = statisticsInMock.commentsCount() / statisticsInMock.userNames().size();
+        double commentsByUser = morePostsThanComments.averageQuantityOfCommentsByUsers();
+        Assert.assertEquals(averageOfCommentsWhichMustBeExpected, commentsByUser, 1);
+
+        int averageOfCommentsByPostsWhichMustBeExpected = statisticsInMock.commentsCount() / statisticsInMock.postCount();
+        double commentsByPosts = morePostsThanComments.averageQuantityOfPostsByUsers();
+        Assert.assertEquals(averageOfCommentsByPostsWhichMustBeExpected, commentsByPosts, 1);
+    }
+
+    @Test
+    public void testOfStatisticsWhenIsSecondTypeOfInformation()
+    {
+        //Given
+        Statistics statisticsInMock = mock(Statistics.class);
+
+        int quantityOfPosts = 10;
+        int quantityOfComments = 1000;
+
+        ArrayList<String> quantityOfUsers = new ArrayList<>();
+
+        for(int i=0; i<100; i++)
+        {
+            quantityOfUsers.add("User: " + i);
+        }
+
+        when(statisticsInMock.postCount()).thenReturn(quantityOfPosts);
+        when(statisticsInMock.commentsCount()).thenReturn(quantityOfComments);
+        when(statisticsInMock.userNames()).thenReturn(quantityOfUsers);
+
+        //When
+        CalculateStatistics morePostsThanComments = mock(CalculateStatistics.class);
+        morePostsThanComments.calculateAdvStatistics(statisticsInMock);
+
+        //Then
+        int averageOfPostsWhichMustBeExpected = statisticsInMock.postCount() / statisticsInMock.userNames().size();
+        double postsByUser = morePostsThanComments.averageQuantityOfPostsByUsers();
+        Assert.assertEquals(averageOfPostsWhichMustBeExpected, postsByUser, 1);
+
+        int averageOfCommentsWhichMustBeExpected = statisticsInMock.commentsCount() / statisticsInMock.userNames().size();
+        double commentsByUser = morePostsThanComments.averageQuantityOfCommentsByUsers();
+        Assert.assertEquals(averageOfCommentsWhichMustBeExpected, commentsByUser, 1);
+
+        int averageOfCommentsByPostsWhichMustBeExpected = statisticsInMock.commentsCount() / statisticsInMock.postCount();
+        double commentsByPosts = morePostsThanComments.averageQuantityOfCommentsByPosts();
+        Assert.assertEquals(averageOfCommentsByPostsWhichMustBeExpected, commentsByPosts, 1);
+    }
+}
