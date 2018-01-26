9aa0893acb531bacaa8ce43843a0309a04951a24
programtica <kondmo@gmail.com>
2018-01-26 19:33:15 +0100

    Zadanie 7.6

--- kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/portfolio/BoardTestSuite.java
@@ -2,7 +2,6 @@ package com.kodilla.stream.portfolio;
 
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
@@ -19,7 +18,6 @@ public class BoardTestSuite
         User user2 = new User("projectmanager1", "Nina White");
         User user3 = new User("developer2", "Emilia Stephanson");
         User user4 = new User("developer3", "Konrad Bridge");
-
         //tasks
         Task task1 = new Task("Microservice for taking temperature",
                 "Write and test the microservice taking\n" +
@@ -28,7 +26,6 @@ public class BoardTestSuite
                 user2,
                 LocalDate.now().minusDays(20),
                 LocalDate.now().plusDays(30));
-
         Task task2 = new Task("HQLs for analysis",
                 "Prepare some HQL queries for analysis",
                 user1,
@@ -59,7 +56,6 @@ public class BoardTestSuite
                 user2,
                 LocalDate.now().minusDays(15),
                 LocalDate.now().minusDays(2));
-
         //taskLists
         TaskList taskListToDo = new TaskList("To do");
         taskListToDo.addTask(task1);
@@ -70,6 +66,7 @@ public class BoardTestSuite
         taskListInProgress.addTask(task2);
         TaskList taskListDone = new TaskList("Done");
         taskListDone.addTask(task6);
+
         //board
         Board project = new Board("Project Weather Prediction");
         project.addTaskList(taskListToDo);
