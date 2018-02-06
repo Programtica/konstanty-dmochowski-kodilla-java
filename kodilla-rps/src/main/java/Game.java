a7fba675a79e8c7dbce3753a2ceb077364e1247d
programtica <kondmo@gmail.com>
2018-02-06 20:06:45 +0100

    Changes

--- kodilla-rps/src/main/java/Game.java
+++ kodilla-rps/src/main/java/Game.java
@@ -1,42 +1,46 @@
-//TO IMPROVE...git
+//First attempt :)
 
 import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
-    private String name;
+    private String username;
     private int numberOfRounds;
     private int userResult;
     private int computerResult;
     private char key;
     private int generatedNumber;
+    private Scanner scanner;
 
-    public Game(String nameOfApplication) {
-        name = "";
+    public Game() {
+        username = "";
         numberOfRounds = 0;
         key = '0';
         userResult = 0;
         computerResult = 0;
         generatedNumber = 0;
-        System.out.println(nameOfApplication);
+        scanner = new Scanner(System.in);
+    }
+
+    public void showANameOfApplication(String usernameOfApplication)
+    {
+        System.out.println(usernameOfApplication);
     }
 
     public void enterInput() {
-        System.out.print("\nWhat's Your name? ");
-        Scanner text = new Scanner(System.in);
-        name = text.next();
+        System.out.print("\nWhat's Your username? ");
+        username = scanner.next();
 
-        System.out.println("Hello " + name + "!");
+        System.out.println("Hello " + username + "!");
 
         System.out.print("\nEnter number of rounds to play: ");
-        Scanner number = new Scanner(System.in);
-        numberOfRounds = number.nextInt();
+        numberOfRounds = scanner.nextInt();
     }
 
     public void showInformationAboutKeys() {
-        System.out.println(name + ", You want to play " + numberOfRounds
+        System.out.println(username + ", You want to play " + numberOfRounds
                 + " rounds? Sound nice!");
-        System.out.println("\nOk " + name + ", this is an information about keys: \n");
+        System.out.println("\nOk " + username + ", this is an information about keys: \n");
         System.out.println("1 - Rock" + "\n" + "2 - Paper" + "\n" + "3 - Scissors"
         + "\n" + "x - End Game" + "\n" + "n - New Game");
         System.out.println("\nEnter one of them, and something will happen!");
@@ -44,18 +48,17 @@ public class Game {
 
     public void play() {
         for(int i=1; i<=numberOfRounds; i++) {
-            System.out.print("\nRound " + i + ": " + name + ", let's go! What's Your move? ");
-            Scanner move = new Scanner(System.in);
-            key = move.next().charAt(0);
+            System.out.print("\nRound " + i + ": " + username + ", let's go! What's Your move? ");
+            key = scanner.next().charAt(0);
 
             switch (key) {
                 case '1':
                 case '2':
                 case '3':
-                    System.out.println(name + ", You entered " + key + ". Wait a moment!");
+                    System.out.println("\n" + username + ", You entered " + key + ". Wait a moment!");
                     Random rand = new Random();
                     generatedNumber = rand.nextInt((2) + 1) + 1;
-                    System.out.println("\nComputer entered " + generatedNumber + ". Hmm... what's the result?");
+                    System.out.println("Computer entered " + generatedNumber + ". Hmm... what's the result?");
                     resultOfGame();
                     break;
 
@@ -79,65 +82,46 @@ public class Game {
         if((key=='3' && generatedNumber==2) || (key=='2' && generatedNumber == 1) ||
                 (key=='1' && generatedNumber == 3)) {
             userResult++;
-        }
-
-        else if((key=='1' && generatedNumber == 1) ||
+            System.out.println("\nIn that round win: " + username);
+        } else if((key=='1' && generatedNumber == 1) ||
                 (key=='2' && generatedNumber == 2 || key=='3' && generatedNumber == 3)) {
-            System.out.println("Current result: draw... Next turn!");
+            System.out.println("\nResult: draw... Next turn!");
+        } else {
+            computerResult++;
+            System.out.println("\nIn that round win: Computer");
         }
 
-        else
-            computerResult++;
-
-        System.out.println("Current result " + name + "-Computer: " + userResult + ":" + computerResult);
+        System.out.println("Current result " + username + "-Computer: " + userResult + ":" + computerResult);
         checkWin();
     }
 
     public void checkWin() {
-        int specialCaseComputer = computerResult-userResult;
-        int specialCaseUser = userResult-computerResult;
-
         if(userResult == numberOfRounds) {
-            System.out.println("Win: " + name);
+            System.out.println("Win: " + username);
             decision();
-        }
-
-        else if(computerResult == numberOfRounds) {
+        } else if(computerResult == numberOfRounds) {
             System.out.println("Win: Computer");
             decision();
-        }
-
-        else if(userResult == computerResult || (key=='1' && generatedNumber == 1) ||
-                (key=='2' && generatedNumber == 2 || key=='3' && generatedNumber == 3)) {
-            numberOfRounds++;
-        }
-
-        else if(specialCaseUser >= 2 && numberOfRounds >= specialCaseUser) {
-            System.out.println(name + ", You can win it!");
-            numberOfRounds++;
-        }
-
-        else if(specialCaseComputer >= 2 && numberOfRounds >= specialCaseComputer) {
-            System.out.println("Computer, You can win it!");
-            numberOfRounds++;
+        } else {
+            System.out.println("Very good!");
         }
     }
 
     public void decision() {
-
         computerResult = 0;
         userResult = 0;
         numberOfRounds = 0;
 
-        System.out.print("\nWhat is your decision? [x - Exit Game, n - New Game]");
-        Scanner move = new Scanner(System.in);
-        key = move.next().charAt(0);
+        System.out.print("\nWhat is your decision? [x - Exit Game, n - New Game] ");
+        key = scanner.next().charAt(0);
+        scanner.close();
 
         switch (key) {
             case 'x':
             case 'X':
                 endGame();
                 break;
+
             case 'N':
             case 'n':
                 newGame();
@@ -147,13 +131,13 @@ public class Game {
 
     public void endGame() {
         System.out.print("Do You want leave a game? [Y - yes, N - no] ");
-        Scanner ending = new Scanner(System.in);
-        char question = ending.next().charAt(0);
+        char question = scanner.next().charAt(0);
+        scanner.close();
 
         switch(question) {
             case 'y':
             case 'Y':
-                System.out.println("Bye bye! See You soon, " + name);
+                System.out.println("Bye bye! See You soon, " + username);
                 break;
 
             case 'n':
@@ -168,9 +152,9 @@ public class Game {
     }
 
     public void newGame() {
-        System.out.print("Do You want start new game? [Y - yes, N - no]");
-        Scanner starting = new Scanner(System.in);
-        char question = starting.next().charAt(0);
+        System.out.print("Do You want start new game? [Y - yes, N - no] ");
+        char question = scanner.next().charAt(0);
+        scanner.close();
 
         switch(question) {
             case 'y':
@@ -182,7 +166,7 @@ public class Game {
 
             case 'n':
             case 'N':
-                System.out.println("Nope!");
+                System.out.println("Why so serious!");
                 numberOfRounds--;
                 break;
 
--- kodilla-rps/src/main/java/RpsRunner.java
+++ kodilla-rps/src/main/java/RpsRunner.java
@@ -2,9 +2,10 @@ public class RpsRunner {
     public static void main(String[] args) {
         boolean end = false;
         String nameOfApplication = "ROCK-PAPER-SCISSORS";
-        Game game = new Game(nameOfApplication);
+        Game game = new Game();
 
         while(!end) {
+            game.showANameOfApplication(nameOfApplication);
             game.enterInput();
             game.showInformationAboutKeys();
             game.play();
