9397a906901e3fe6b9e61449ca186dbcf658a05b
programtica <kondmo@gmail.com>
2018-02-14 12:54:14 +0100

    Zadanie 9.4

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/ResultOfProcess.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/ResultOfProcess.java
@@ -2,5 +2,5 @@ package com.kodilla.good.patterns.challenges.food2door;
 
 public class ResultOfProcess {
     public static final String SUCCESS = "Zamówienie zostało zrealizowane pomyślnie!\n";
-    public static final String FAILURE = "Zamówienie jeszcze trwa!\n";
+    public static final String FAILURE = "Zamówienie jeszcze trwa lub miało jakieś błędy!\n";
 }
