1117aeb9eaf98bb8a99881b67ba52ee9079246a8
97kos <kondmo@gmail.com>
2018-05-08 19:08:58 +0200

    Zadanie 20.1

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -38,7 +38,7 @@ public class DbManagerTestSuite {
         }
         rs.close();
         statement.close();
-        Assert.assertEquals(5, counter);
+        Assert.assertEquals(0, counter);
     }
 
     @Test
@@ -66,6 +66,6 @@ public class DbManagerTestSuite {
         }
         rs.close();
         statement.close();
-        Assert.assertEquals(1, counter);
+        Assert.assertEquals(0, counter);
     }
 }
